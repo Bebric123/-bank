e54828485d2714cb65c757546c244da9df865399
= <swooplida@gmail.com>
2025-03-25 17:07:28 +0500

    оформление отчетов

--- financebank/integrations/views.py
+++ financebank/integrations/views.py
@@ -1,5 +1,6 @@
 import csv
 from datetime import datetime
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
@@ -8,15 +9,22 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def import_csv(request):
-    if request.method == "POST" and request.FILES["csv_file"]:
-        csv_file = request.FILES["csv_file"]
+    if request.method == "POST":
+        csv_file = request.FILES.get("csv_file")
+
+        if not csv_file:
+            messages.error(request, "Ошибка: Выберите CSV-файл перед загрузкой.")
+            return redirect("import_csv")
+
         decoded_file = csv_file.read().decode("utf-8").splitlines()
         reader = csv.DictReader(decoded_file)
+
         print(f"Заголовки в CSV: {reader.fieldnames}")
 
         missing_columns = {"transaction_type", "category", "amount", "date"} - set(reader.fieldnames)
         if missing_columns:
-            return HttpResponse(f"Ошибка: отсутствуют столбцы {', '.join(missing_columns)}", status=400)
+            messages.error(request, f"Ошибка: отсутствуют столбцы {', '.join(missing_columns)}")
+            return redirect("import_csv")
 
         records = []
         for row in reader:
@@ -30,9 +38,11 @@ def import_csv(request):
                 )
                 records.append(record)
             except KeyError as e:
-                return HttpResponse(f"Ошибка: отсутствует поле {str(e)} в CSV", status=400)
+                messages.error(request, f"Ошибка: отсутствует поле {str(e)} в CSV")
+                return redirect("import_csv")
         
         FinanceRecord.objects.bulk_create(records)
-        return HttpResponse("Импорт завершен!")
+        messages.success(request, "Импорт завершен!")
+        return redirect("import_csv")
 
     return render(request, "integrations/import_csv.html")
